28d815e3f8ed322f06b05c0d9fb32b7e48cf40e5
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-08 23:36:35 -0700

    changed file loc and added 404 page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,6 +6,10 @@ const app = express()
 app.get('/', (req, res)=> {
     res.send('Hello world!')
 })
-app.listen(3000, function () {
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT, function () {
     console.log('listening on Port 3000')
 })
