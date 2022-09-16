616efd3eb62c7ea3035b5252b03becd8ec885dd6
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-16 13:17:31 -0700

    New places can now be added

---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/lights.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/Coffee.jpg'
+  }]  
+  
