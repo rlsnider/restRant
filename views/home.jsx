170bbc894ec79f0105b685552ec4f5dab563d16b
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-17 18:57:38 -0700

    added NavBar and styling

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,13 @@ function home () {
     return (
     <Def>
         <main>
-            <h1>REST-Rant</h1>
-            <div>
-                <div>
-                <img src="/images/ChiaFruit.jpg" alt="Chia Fruit Shake" width="40%" />
-                <span>Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on <a href="//unsplash.com/photos/MsTOg6rhRVk">Unsplash</a></span>
-                </div>
+            <div id="homepage">
+                <h1>REST-Rant</h1>
+                    <div>
+                        <img src="/images/ChiaFruit.jpg" alt="Chia Fruit Shake" width="50%" /><br />
+                        <span>Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on <a href="//unsplash.com/photos/MsTOg6rhRVk">Unsplash</a></span>
+                    </div>
             </div>
-            <a href="/places">
-                <button className="btn btn-primary">Places Page</button>
-            </a>
         </main>
     </Def>
     )
