23942ce34519614d54c58bbf36c8e1758fdde97c
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-25 12:21:29 -0700

    added helper method

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404 () {
                 <div id='errorDivOne'>
                     <h1>404: PAGE NOT FOUND</h1>
                 
-                <img src="/images/error404.jpg" alt="Ang(dog) looking at you" width='50%' />
+                <img id='dogImage' src="/images/error404.jpg" alt="Ang(dog) looking at you" width='50%' />
                 <p>Oops, sorry, we can't find this page!</p>
                 </div>
             </main>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,18 +6,24 @@ function show (data) {
         <Def>
             <main>
 
-                <h1>{data.place.name}</h1>
+            <h1>{data.place.name}</h1>
                 <h3>{data.cuisines}</h3>
                 <div className="row">
                     <div className="col-sm-6" >
-                        <img id="showBorder" src={data.place.pic} alt={data.place.name} width="80%" />
+                        <img id="showBorder" src={data.place.pic} alt={data.place.name} width="45%" />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
                     </div>
                      <div className="col-sm-6">  
                         <h2>Rating</h2>
                         <h2>Description</h2>
-                        <p>
-                            Located in {data.place.city}, {data.place.state}
-                        </p>
+                            <h3>
+                                {data.place.showEstablished()}
+                            </h3>  
+                            <h4>
+                                Serving {data.place.cuisines}
+                            </h4>
                     </div>
                     <div>
                         <h1>Comments</h1>
